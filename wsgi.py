79e88333d2b7d0cf82a5fc4960a5e5e7f53ff536
Shulhi Sapli <shulhi@gmail.com>
2015-06-25 13:59:16 +0800

    Remove activate virtualenv from wsgi application.

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,3 @@
-import sys, os, pwd
-
-BASE_DIR = os.path.join(os.path.dirname(__file__))
-
-# activate virtualenv
-activate_this = os.path.join(BASE_DIR, "env/bin/activate_this.py")
-execfile(activate_this, dict(__file__=activate_this))
-
-if BASE_DIR not in sys.path:
-    sys.path.append(BASE_DIR)
-
 # give wsgi the application
 from app import create_app
 application = create_app()
